0303823dd18ba23e733d4fbc88cbad7f2292b5ac
Estudiante <Estudiante@T203-17-2022>
2022-10-18 21:39:19 +0200

    Agregar archivos de proyecto.

---
+++ lert_reto_1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaVector2
+{
+    class PruebaVector2
+    {
+        private float[] notas;
+        private float promedio;
+
+        public void Cargar()
+        {
+            notas = new float[15];
+            for (int f = 0; f < 15; f++)
+            {
+                Console.Write("Ingrese las notas de los alumnos:");
+                string linea = Console.ReadLine();
+                notas[f] = float.Parse(linea);
+            }
+        }
+        public void NotasTotales()
+        {
+            float total = 0;
+            for (int f = 0; f < 15; f++)
+            {
+                total = total + notas[f];
+            }
+            Console.WriteLine("Total de notas de los alumnos:" + total);
+            Console.ReadKey();
+        }
+
+        public void CalcularPromedio()
+        {
+            float suma;
+            suma = 0;
+            for (int f = 0; f < 15; f++)
+            {
+                suma = suma + notas[f];
+            }
+            promedio = suma / 15;
+            Console.WriteLine("Promedio de las notas de los alumnos :" + promedio);
+        }
+
+        public void MayoresMenores()
+        {
+            int may, men;
+            may = 0;
+            men = 0;
+            for (int f = 0; f < 15; f++)
+            {
+                if (notas[f] > promedio)
+                {
+                    may++;
+                }
+                else
+                {
+                    if (notas[f] < promedio)
+                    {
+                        men++;
+                    }
+                }
+            }
+            Console.WriteLine("Cantidad de personas mayores al promedio:" + may);
+            Console.WriteLine("Cantidad de personas menores al promedio:" + men);
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            PruebaVector2 pv2 = new PruebaVector2();
+            pv2.Cargar();
+            pv2.CalcularPromedio();
+            pv2.MayoresMenores();
+            pv2.NotasTotales
+                ();
+        }
+    }
+}
---
+++ lert_reto_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("lert_reto_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lert_reto_1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("268474e8-4995-4e96-a6e0-b419152835c5")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
